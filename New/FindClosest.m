836590988210d3702d783884c4e7da2ffb91196a
raltodo <ralitsa.todorova@college-de-france.fr>
2025-01-28 12:09:41 +0100

    apply updates from 2019

--- New/FindClosest.m
+++ New/FindClosest.m
@@ -1,29 +1,50 @@
 function [indices,values] = FindClosest(reference, query, mode)
 
-% This function looks up a query vector in a reference vector, and for each
-% value in the query vector, returns the index of the closest value in the
-% reference vector. The equivalent non-optimised code is trivial:
-% for i=1:length(reference), indices(i,1) = find(abs(query-reference(i)) == min(abs(query-reference(i))), 1, 'first'); end
-% EXAMPLE 1 (reasoning):
-% FindClosest([0.5; 0.2],[0.3 0.45 0.66]) returns [2;1]
-% as the closest value to 0.5 was 0.45 (index 2), and to 0.2 was 0.3 (index 1)
-% EXAMPLE 2 (usage):
-% indices = FindClosests(spikes1,spikes2) will return the indices of spikes2 closest to spikes1
-% spikes1 - spikes2(indices) will contain all the minimal distances between spikes1 and spikes2 (one distance per each spike in spike1)
+%FindClosest - field the indices of a reference vector closest to a query vector
+% Look up a query vector in a reference vector, and for each value in 
+% the query vector, return the index of the closest value in the
+% reference vector. 
 %
-% (C) 2016 by Ralitsa Todorova
+% USAGE
+%
+%    [indices,values] = FindClosest(reference, query, mode)
+%
+%    reference      reference signal which will be used as the table look-up
+%    query          query signal. For each value of this signal, the function
+%                   will look for the closest value in the reference signal
+%    mode           a string ('either','higher', or 'lower') indicating 
+%                   whether the function should find the closest value in
+%                   reference higher than the query ('higher) or lower than
+%                   the query ('lower') or the closest value regardless of
+%                   the direction of the difference (default = 'either')
+%
+%
+% EXAMPLE
+% indices = FindClosests(spikes(:,1),deltas(:,2)) will return the indices of spikes closest to the delta wave peak
+% d = deltas(:,2) - spikes(indices) will contain all the minimal distances between delta wave peaks and spikes (one distance per each delta wave)
+%
+% NOTE 
+% This is an optimized implementation of computing the following trivial code:
+% for i=1:length(reference), indices(i,1) = find(abs(query-reference(i)) == min(abs(query-reference(i))), 1, 'first'); end
+% values = reference(indices);
+%
+% (C) 2019 by Ralitsa Todorova
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
-if ~exist('mode','var'),mode = 'either'; end
 
+if ~exist('mode','var'),mode = 'either'; end
 if isempty(reference) || isempty(query), indices = []; values = []; return; end
 if length(reference)==1,indices = 1; values=reference; return; end
 
+nans = isnan(reference);
+nonnans = find(~nans);
+reference(nans) = [];
+
 % Make sure values to be matched are unique
-[u,i] = unique(reference); nans = isnan(u); u(nans) = []; i(nans) =[];
+[u,i] = unique(reference);
 % Find closest index
 if strcmp(mode,'higher'),
     indices = ceil(interp1(u,(1:length(u))',query));
@@ -40,3 +61,4 @@ indices(indices>length(i) | query>max(u)) = length(i);
 
 indices = i(indices);
 values = reference(indices);
+indices = nonnans(indices);
