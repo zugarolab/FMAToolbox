8cd54d123898ed487469fbe97ceccfcbdcd64eca
raltodo <ralitsa.todorova@college-de-france.fr>
2025-04-10 19:20:50 +0200

    fix annoying default placing NaNs first

--- Helpers/findmax.m
+++ Helpers/findmax.m
@@ -13,7 +13,7 @@ function [indices,values] = findmax(data,k)
 if nargin<2
     [values,indices] = max(data);
 else
-    [values,order] = sort(data,'descend');
+    [values,order] = sort(data,'descend','MissingPlacement','last');
     indices = order(1:k);
     values = values(1:k);
 end
