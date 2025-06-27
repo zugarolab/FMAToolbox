c3d90ad3154ed236bede5dc1ea2474eef7f588a7
pietro <pietro.bozzo1998@gmail.com>
2025-06-27 14:42:54 +0200

    force input to be column vector (also automatically convert row vectors to columns)

--- New/linspaceVector.m
+++ New/linspaceVector.m
@@ -7,13 +7,18 @@ function y = linspaceVector(d1, d2)
 %
 % Example:
 % linspaceVector([2;5;23],[2;8;23]) = [2;5;6;7;8;23]
-%
+
 % Copyright (C) 2019 by Ralitsa Todorova
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
+
+arguments
+    d1 (:,1)
+    d2 (:,1)
+end
 
 n = d2-d1;
 
