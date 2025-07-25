709cbb724f8482bd78997007ef24643ff9b73902
pietro <pietro.bozzo1998@gmail.com>
2025-07-25 14:26:49 +0200

    add option step to implement overlapping windows

--- New/FiringRatio.m
+++ New/FiringRatio.m
@@ -16,6 +16,8 @@ function ratio = FiringRatio(spikes,opt)
 %     Properties    Values
 %    -------------------------------------------------------------------------
 %     'binSize'     bin size in s (default is 0.05)
+%     'step'        ratio is computed in windows of length 'binSize' and overlap
+%                   'binSize' / 'step' (must be integer, default is 1: no overlap)
 %     'limits'      [start stop] in s (default is ~ first and last spike times)
 %     'smooth'      gaussian kernel std in number of samples, default is 1
 %                   (no smoothing)
@@ -23,7 +25,7 @@ function ratio = FiringRatio(spikes,opt)
 %
 %  OUTPUT
 %
-%    ratio          each row is [t, istantaneuous ratio]
+%    ratio          each row is [t, istantaneous ratio]
 %
 %  SEE
 %
@@ -39,6 +41,7 @@ function ratio = FiringRatio(spikes,opt)
 arguments
     spikes (:,2) {mustBeNumeric}
     opt.binSize (1,1) {mustBeNumeric,mustBePositive} = 0.05
+    opt.step (1,1) {mustBeNumeric,mustBeInteger,mustBePositive} = 1
     opt.limits (2,1) {mustBeNumeric} = [NaN,NaN]
     opt.smooth (1,1) {mustBeNumeric,mustBePositive} = 1
 end
@@ -50,18 +53,24 @@ opt.limits(isnan(opt.limits)) = default_limits(isnan(opt.limits));
 % keep spikes inside limits and shift them to 0
 spike_times = spikes(:,1);
 keep_ind = spike_times >= opt.limits(1) & spike_times <= opt.limits(2);
-spike_times = spike_times(keep_ind);
+spikes = spikes(keep_ind,:);
+spike_times = spikes(:,1);
 spike_times = spike_times - opt.limits(1) + opt.binSize/2;
 
-% discretize time
-bin_ind = ceil(spike_times / opt.binSize);
-bin_ind(bin_ind==0) = 1;
-
 % ratio
+time_bins = opt.limits(1) : opt.binSize : opt.limits(2)+opt.binSize/2;
+t = zeros(opt.step,numel(time_bins));
+ratio = zeros(opt.step,numel(time_bins));
 n_units = numel(unique(spikes(:,2)));
-ratio = accumarray(bin_ind,spikes(keep_ind,2),[],@(x) numel(unique(x))) / n_units;
-time = (opt.limits(1) : opt.binSize : opt.limits(2)+opt.binSize/2).';
-ratio = [time, [ratio;zeros(numel(time)-numel(ratio),1)]];
+for i = 1 : opt.step
+    % discretize time
+    bin_ind = ceil((spike_times - (i-1)*opt.binSize/opt.step) / opt.binSize);
+    bin_ind(bin_ind==0) = 1;
+
+    ratio(i,:) = accumarray(bin_ind,spikes(:,2),[numel(time_bins),1],@(x) numel(unique(x))).' / n_units;
+    t(i,:) = time_bins + (i-1)*opt.binSize/opt.step;
+end
+ratio = [t(:),ratio(:)];
 
 % smoothing
 ratio(:,2) = smoothdata(ratio(:,2),'gaussian',opt.smooth);
