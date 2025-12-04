450076fb9a2e56c4740c6626494f21312cfc3106
pietro <pietro.bozzo1998@gmail.com>
2025-12-04 16:04:59 +0100

    use cell arrays for speed optimization (~3x), add sample index output

--- Analyses/Sync.m
+++ Analyses/Sync.m
@@ -1,4 +1,4 @@
-function [synchronized,indices] = Sync(samples,sync,varargin)
+function [synchronized,Ie,Is] = Sync(samples,sync,varargin)
 
 %Sync - Make sample timestamps relative to synchronizing events.
 %
@@ -8,7 +8,7 @@ function [synchronized,indices] = Sync(samples,sync,varargin)
 %
 %  USAGE
 %
-%    [synchronized,indices] = Sync(samples,sync,<options>)
+%    [synchronized,Ie,Is] = Sync(samples,sync,<options>)
 %
 %    samples        <a href="matlab:help samples">samples</a> to synchronize
 %    sync           timestamps to synchronize on (e.g., brain stimulations)
@@ -28,12 +28,13 @@ function [synchronized,indices] = Sync(samples,sync,varargin)
 %  OUTPUT
 %
 %    synchronized   resynchronized samples
-%    indices        for each sample, the corresponding synchronizing event index
+%    Ie             index of synchronizing event for each resynchronized sample
+%    Is             indeces of resynchronized samples in original samples 
 %
 %  EXAMPLE
 %
 %    [raster,indices] = Sync(spikes,stimuli);     % compute spike raster data
-%    figure;PlotSync(raster,indices);             % plot spike raster
+%    figure; PlotSync(raster,indices);            % plot spike raster
 %
 %  SEE
 %
@@ -63,30 +64,30 @@ end
 
 % Parse parameter list
 for i = 1:2:length(varargin)
-	if ~ischar(varargin{i})
-		error(['Parameter ' num2str(i+2) ' is not a property (type ''help <a href="matlab:help Sync">Sync</a>'' for details).']);
-	end
-	switch lower(varargin{i})
-		case 'durations'
-			durations = varargin{i+1};
-			if ~isdvector(durations,'#2','<=')
-				error('Incorrect value for property ''durations'' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).');
-      end
-    case 'fast'
-      fast = varargin{i+1};
-      if ~isastring(fast,'on','off')
-				error('Incorrect value for property ''fast'' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).');
-			end
-			fast = strcmp(fast,'on');
-		case 'verbose'
-			verbose = varargin{i+1};
-			if ~isastring(verbose,'on','off')
-				error('Incorrect value for property ''verbose'' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).');
-			end
-			verbose = strcmp(verbose,'on');
-    otherwise
-			error(['Unknown property ''' num2str(varargin{i}) ''' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).']);
-	end
+    if ~ischar(varargin{i})
+		    error(['Parameter ' num2str(i+2) ' is not a property (type ''help <a href="matlab:help Sync">Sync</a>'' for details).']);
+    end
+    switch lower(varargin{i})
+        case 'durations'
+            durations = varargin{i+1};
+            if ~isdvector(durations,'#2','<=')
+                error('Incorrect value for property ''durations'' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).');
+            end
+        case 'fast'
+            fast = varargin{i+1};
+            if ~isastring(fast,'on','off')
+                error('Incorrect value for property ''fast'' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).');
+            end
+            fast = strcmp(fast,'on');
+        case 'verbose'
+            verbose = varargin{i+1};
+            if ~isastring(verbose,'on','off')
+                error('Incorrect value for property ''verbose'' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).');
+            end
+            verbose = strcmp(verbose,'on');
+        otherwise
+            error(['Unknown property ''' num2str(varargin{i}) ''' (type ''help <a href="matlab:help Sync">Sync</a>'' for details).']);
+    end
 end
 
 % Make sure samples and sync events are sorted in time
@@ -96,41 +97,19 @@ if ~fast
 end
 
 nSync = length(sync);
+Is = cell(nSync,1);
+previous = 1; % index of first sample found in previous cycle
 verbose && fprintf(1,[num2str(nSync) ' synchronizing events to process...\n']);
-
-% Output matrices will be allocated in arbitrarily large blocks (and will be trimmed down later)
-% This will be much faster than increasing matrix size by the exact appropriate amount for each synchronizing event
-blockLength = 1e6;
-synchronized = nan(blockLength,size(samples,2));
-indices = nan(blockLength,1);
-
-% This variable is used to speed up computations: to find points within each sync time window, start from the beginning
-% of the previous sync window rather than from the very beginning of the sample data
-previous = 1;
-
-k = 1;
 for i = 1 : nSync
-
-	% Find samples within time window around this synchronizing event
-	j = FindInInterval(samples,[sync(i)+durations(1) sync(i)+durations(2)],previous);
-	if ~isempty(j)
-
-		previous = j(1);
-		j = (j(1):j(2))';
-		nj = length(j);
-    verbose && fprintf(1,[' sync ' int2str(i) ' (t=' num2str(sync(i)) '): ' int2str(nj) ' samples\n']);
-
-		% Synchronize samples
-    if length(indices) < k + nj
-			% Enlarge matrices if necessary
-			synchronized = [synchronized;nan(blockLength,size(samples,2))];
-			indices = [indices;nan(blockLength,1)];
-		end
-		synchronized(k:k+nj-1,:) = [samples(j,1)-sync(i) samples(j,2:end)];
-		indices(k:k+nj-1,1) = i*ones(size(j));
-
-		k = k + nj;
-	end
+    % Find samples within time window around this synchronizing event
+  	j = FindInInterval(samples,[sync(i)+durations(1) sync(i)+durations(2)],previous);
+    if ~isempty(j)
+        previous = j(1);
+        Is{i} = (j(1) : j(2)).';
+        verbose && fprintf(1,[' sync ' int2str(i) ' (t=' num2str(sync(i)) '): ' int2str(length(j)) ' samples\n']);
+    end
 end
-indices(k:end) = [];
-synchronized(k:end,:) = [];
+
+Ie = repelem((1:nSync).',cellfun(@numel,Is),1);
+Is = vertcat(Is{:});
+synchronized = [samples(Is,1)-sync(Ie),samples(Is,2:end)];
