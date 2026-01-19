c647a3ed1f07b3462436a8e1e42db5ce3f695235
pietro <pietro.bozzo1998@gmail.com>
2026-01-19 16:51:42 +0100

    add option ax to choose axes to plot on

--- Plot/PiecewiseLinearAxis.m
+++ Plot/PiecewiseLinearAxis.m
@@ -1,16 +1,23 @@
-function a = PiecewiseLinearAxis(x,axis,varargin)
+function ax = PiecewiseLinearAxis(x,axis,varargin)
 
 %PiecewiseLinearAxis - Label axis using piecewise linear values.
 %
 %  USAGE
 %
-%    a = PiecewiseLinearAxis(x,axis,<options>)
+%    ax = PiecewiseLinearAxis(x,axis,<options>)
 %
-%    x              axis values
-%    axis           optional axis: 'x' or 'y' (default = 'x')
-%    <options>      options for function <a href="matlab:help plot">plot</a>
+%    x                axis values
+%    axis             optional axis: 'x' or 'y' (default = 'x')
+%    <options>        optional list of property-value pairs (see table below)
+%
+%    =========================================================================
+%     Properties      Values
+%    -------------------------------------------------------------------------
+%     'ax'            axes to operate on (default = gca)
+%     <plot options>  any other property (and all properties that follow)
+%                     will be passed to <a href="matlab:help xline">plot</a>
 
-% Copyright (C) 2015 by Michaël Zugaro
+% Copyright (C) 2015 by Michaël Zugaro, 2026 by Pietro Bozzo (options)
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -18,33 +25,38 @@ function a = PiecewiseLinearAxis(x,axis,varargin)
 % (at your option) any later version.
 
 % Check number of parameters
-if nargin < 1,
-  error('Incorrect number of parameters (type ''help <a href="matlab:help PiecewiseLinearAxis">PiecewiseLinearAxis</a>'' for details).');
+if nargin < 1
+    error('Incorrect number of parameters (type ''help <a href="matlab:help PiecewiseLinearAxis">PiecewiseLinearAxis</a>'' for details).');
 end
 
 % Check parameters
-if ~isdvector(x,'<'),
-  error('Incorrect values (type ''help <a href="matlab:help PiecewiseLinearAxis">PiecewiseLinearAxis</a>'' for details).');
+if ~isdvector(x,'<')
+    error('Incorrect values (type ''help <a href="matlab:help PiecewiseLinearAxis">PiecewiseLinearAxis</a>'' for details).');
 end
-if nargin < 2,
-	axis = 'x';
+if nargin < 2
+    axis = 'x';
 else
-	axis = lower(axis);
+    axis = lower(axis);
 end
-if ~isastring(axis,'x','y'),
-	varargin = {axis,varargin{:}};
-	axis = 'x';
+if ~isastring(axis,'x','y')
+    varargin = [{axis},varargin];
+    axis = 'x';
+end
+if isastring(varargin{1},'ax') && numel(varargin) > 1
+    ax = varargin{2};
+    varargin = varargin(3:end);
+else
+    ax = gca;
 end
 if isempty(varargin)
-	varargin = {'k:'};
+    varargin = {'k:'};
 end
 
 % Find transitions (gaps) between linear pieces, and show them graphically
 dx = diff(x);
 binSize = median(dx);
-gaps = [find(dx>2*binSize)+1];
-a = gca;
-set(a,[axis 'lim'],[min(x)-binSize/2 max(x)+binSize/2]);
+gaps = find(dx>2*binSize) + 1;
+set(ax,[axis 'lim'],[min(x)-binSize/2 max(x)+binSize/2]);
 if isempty(gaps), return; end
 
 % Because Matlab uses linear axes, we will need a reference vector X starting at x(1), ending at x(end),
@@ -52,28 +64,28 @@ if isempty(gaps), return; end
 X = linspace(x(1),x(end),length(x))';
 
 % Graphically indicate gap locations
-if strcmp(axis,'x'),
-	PlotHVLines(X(gaps),varargin{:});
+if strcmp(axis,'x')
+    xline(ax,X(gaps),varargin{:})
 else
-	PlotHVLines(X(gaps),'h',varargin{:});
+    yline(ax,X(gaps),varargin{:})
 end
 
 % We will ask Matlab to set optimal ticks for each linear piece
 % To this end, we create temporary, invisible axes of the same physical size (in the figure window)
 % as each piece, set its limits, and get tick information from there; we then transfer these back
 % to the original axis
-P = get(a,'position');
+P = get(ax,'position');
 gaps = [1;gaps;length(x)];
 ticks = [];
 tickLabels = [];
-for i = 2:length(gaps),
+for i = 2:length(gaps)
 	% Determine location of start/stop of current piece, in Matlab 'coordinates' (i.e. using X)
 	start = X(gaps(i-1));
 	stop = X(gaps(i));
 	% Create temporary, invisible axes, and set their physical width (in the figure window) so that
 	% it matches that of current linear piece
-	if strcmp(axis,'x'),
-		factor = P(3)/(x(end)-x(1))
+	if strcmp(axis,'x')
+		factor = P(3)/(x(end)-x(1));
 		p = [0 0.25 (stop-start)*factor 0.5];
 	else
 		factor = P(4)/(x(end)-x(1));
@@ -92,4 +104,4 @@ for i = 2:length(gaps),
 	delete(tmp);
 end
 % Set tick locations and labels
-set(a,[axis 'tick'],ticks,[axis 'TickLabel'],tickLabels);
+set(ax,[axis 'tick'],ticks,[axis 'TickLabel'],tickLabels);
