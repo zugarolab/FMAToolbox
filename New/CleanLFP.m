20fc9110ff80016f31d4744bd2ffe73114f55032
pietro <pietro.bozzo1998@gmail.com>
2026-03-31 18:05:37 +0200

    add yline f in gui, implement argument block

--- New/CleanLFP.m
+++ New/CleanLFP.m
@@ -1,5 +1,5 @@
-function [clean, bad, badIntervals] = CleanLFP(lfp, varargin)
-% CleanLFP: removes artefacts from lfp
+function [clean,bad,badIntervals] = CleanLFP(lfp,opt)
+% CleanLFP - Remove artefacts from LFP trace
 %
 % Returns the lfp cleaned from two kinds of artefacts: big artefacts in the
 % lfp signal (surpassing a threshold in z-units) and very fast fluctuations,
@@ -7,20 +7,20 @@ function [clean, bad, badIntervals] = CleanLFP(lfp, varargin)
 %
 %  USAGE
 %
-%    smoothed = CleanLFP(lfp,<options>)
+%    [clean,bad,badIntervals] = CleanLFP(lfp,<options>)
 %
-%    data           lfp to clean in [timestamps values] format, where timestamps
-%                   are expected to be in secods.
-%    <options>      optional list of property-value pairs (see table below))
+%    data               lfp to clean in [timestamps values] format, where
+%                       timestamps are expected to be in secods.
+%    <options>          optional list of property-value pairs (see table below)
 %
 %    =========================================================================
-%     Properties    Values
+%     Properties        Values
 %    -------------------------------------------------------------------------
-%     'thresholds'  the thresholds for the two signal and derivative artefacts
-%                   in z-units (default = [5 10])
-%     'aroundArtefact'  time around artefact boundaries that should be removed
-%                   (default = [2 0.1]) seconds.
-%     'manual'      choose your thresholds manually in debug mode
+%     'thresholds'      the thresholds for the two signal and derivative
+%                       artefacts in z-units (default = [5 10])
+%     'aroundArtefact'  time around artefact boundaries that should be removed,
+%                       in seconds (default = [2 0.1])
+%     'manual'          choose your thresholds manually in debug mode
 %    =========================================================================
 %
 %  OUTPUT
@@ -30,9 +30,7 @@ function [clean, bad, badIntervals] = CleanLFP(lfp, varargin)
 %                   were flagged as within the artefact and should be removed
 %    badIntervals   the [start stop] intervals in which the signal or derivative
 %                   surpassed its respective threshold
-%
-% Dependencies: ConsolidateIntervals, FindInterval
-%
+
 % Copyright (C) 2017-2022 by Ralitsa Todorova
 %
 % This program is free software; you can redistribute it and/or modify
@@ -40,58 +38,45 @@ function [clean, bad, badIntervals] = CleanLFP(lfp, varargin)
 % the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
 
-% Default values:
-thresholds = [5, 10];
-aroundArtefact = [0.5, 0.1];
-manual = false;
-
-for i = 1:2:length(varargin)
-    if ~ischar(varargin{i})
-        error(['Parameter ', num2str(i+2), ' is not a property (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).']);
-    end
-    switch (lower(varargin{i}))
-        case 'thresholds'
-            thresholds = varargin{i+1};
-            if ~isvector(thresholds) || length(thresholds) ~= 2
-                error('Incorrect value for property ''thresholds'' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).');
-            end
-        case 'aroundartefact'
-            aroundArtefact = varargin{i+1};
-            if ~isvector(aroundArtefact) || length(aroundArtefact) ~= 2
-                error('Incorrect value for property ''aroundArtefact'' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).');
-            end
-        case 'manual'
-            manual = varargin{i+1};
-            if ~islogical(manual) || length(manual) ~= 1
-                error('Incorrect value for property ''manual'' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).');
-            end
-        otherwise
-            error(['Unknown property ''', num2str(varargin{i}), ''' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).']);
-    end
+arguments
+  lfp
+  opt.thresholds (1,2) {mustBeNumeric} = [5,10]
+  opt.aroundArtefact (1,2) {mustBeNumeric} = [0.5,0.1]
+  opt.manual {mustBeGeneralLogical} = false
 end
 
+opt.manual = GeneralLogical(opt.manual);
 
-% parameters
-threshold1 = thresholds(1); % in sigmas deviating from the mean
-aroundArtefact1 = aroundArtefact(1); % 2, Big and long artefacts
+threshold1 = opt.thresholds(1); % in sigmas deviating from the mean
+aroundArtefact1 = opt.aroundArtefact(1); % 2, Big and long artefacts
 
-threshold2 = thresholds(2); % for derivative of z-scored signal
-aroundArtefact2 = aroundArtefact(2); % 0.1 Very fast fluctuations (short time scale)
+threshold2 = opt.thresholds(2); % for derivative of z-scored signal
+aroundArtefact2 = opt.aroundArtefact(2); % 0.1 Very fast fluctuations (short time scale)
 
-t = lfp(:, 1);
-values = lfp(:, 2);
+t = lfp(:,1);
+values = lfp(:,2);
 z = zscore(values);
-d = [diff(z); 0];
+d = [diff(z);0];
 bad = false(size(values));
 
-if manual
-    figure;
-    clf; plot(t, z); hold all; plot(xlim, ones(1, 2)*threshold1, 'r--'); plot(xlim, -ones(1, 2)*threshold1, 'r--'); ylabel('lfp signal (z-units'); legend('signal', 'threshold1'); disp('Feel free to change "threshold1" and type "dbcont" to continue.')
-    keyboard;
-    if false % Optionally, take a look at the threshold for the derivative
-        clf; plot(t, d); hold all; plot(xlim, ones(1, 2)*threshold2, 'r--'); plot(xlim, -ones(1, 2)*threshold2, 'r--'); ylabel('lfp derivative (z-units'); legend('derivative', 'threshold2'); disp('Change "threshold2" manually.')
+if opt.manual
+    figure, hold on
+    plot(t,z);
+    xlabel('time (s)'); ylabel('lfp signal (z-units)');
+    plotThreshold = @(x) yline(x*[-1,1],'r--'); % utility to see new thresholds
+    plotThreshold(threshold1);
+    legend('signal','threshold1');
+    fprintf(1,'Change "threshold1" manually, then type:\n  "plotThreshold(threshold1)" to visualize it,\n  "dbcont" to continue.\n');
+    keyboard
+
+    % Optionally, take a look at the threshold for the derivative
+    if false
+        clf, hold on;
+        plot(t,d); plotThreshold(threshold2); ylabel('lfp derivative (z-units)'); legend('derivative','threshold2');
+        fprintf(1,'Change "threshold2" manually, then type:\n  "plotThreshold(threshold2)" to visualize it,\n  "dbcont" to continue.\n');
     end
 end
+
 % Detect large global artefacts (1)
 artefactInterval = t(FindInterval(abs(z) > threshold1));
 if numel(artefactInterval) == 2
@@ -104,7 +89,7 @@ else
     artefactInterval = zeros(0, 2);
 end
 
-%Find noise using the derivative of the zscored signal (2)
+% Find noise using the derivative of the zscored signal (2)
 noisyInterval = t(FindInterval(abs(d) > threshold2));
 if numel(noisyInterval) == 2
     noisyInterval = noisyInterval(:)';
@@ -122,5 +107,3 @@ values(bad) = interp1(t(~bad), values(~bad), t(bad, 1));
 badIntervals = ConsolidateIntervals(sortrows([artefactInterval; noisyInterval]));
 ok = ~isnan(values);
 clean = [t(ok), values(ok)];
-
-end
