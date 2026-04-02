646717aeec2785c93edc80d97615715cac1d0d40
Pietro <pietro.bozzo1998@gmail.com>
2026-04-02 10:26:55 +0200

    allow empty input

--- New/Unshift.m
+++ New/Unshift.m
@@ -1,7 +1,8 @@
-function timestamps = Unshift(timestamps, intervals)
+function timestamps = Unshift(timestamps,intervals)
 
-% The opposite of the option 'shift' in Restrict
-%
+% The opposite of the option 'shift' in <a href="matlab:help Restrict">Restrict</a> 
+% NOTE: operates only on the first column of 'timestamps', as <a href="matlab:help InIntervals">InIntervals</a> 
+
 % Copyright (C) 2018 Ralitsa Todorova
 %
 % This program is free software; you can redistribute it and/or modify
@@ -9,9 +10,9 @@ function timestamps = Unshift(timestamps, intervals)
 % the Free Software Foundation; either version 3 of the License, or
 % (at your option) any later version.
 
-shiftedIntervals(:,2) = CumSum(diff(intervals,[],2));
+shiftedIntervals(:,2) = cumsum(diff(intervals,[],2));
 shiftedIntervals(:,1) = [0; shiftedIntervals(1:end-1,2)];
 toAdd = intervals(:,1) - shiftedIntervals(:,1);
-[ok,w] = InIntervals(timestamps,shiftedIntervals); 
+[ok,w] = InIntervals(timestamps,shiftedIntervals);
 timestamps(~ok) = nan;
 timestamps(ok) = timestamps(ok) + toAdd(w(ok));
